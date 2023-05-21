50d3b924a98cdb013f78452a7512dd8085afa7ff
Dipta88 <user.name>
2023-05-22 00:26:45 +0600

    added delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -32,7 +32,17 @@ async function run() {
 
 
     const ToyCollection = client.db('toyDB').collection('toys'); 
+     
 
+    app.get('/toys', async(req, res)=>{
+      console.log(req.query.selleremail);
+      let query = {};
+      if(req.query?.selleremail){
+        query = {selleremail: req.query.selleremail }
+      }
+      const result = await ToyCollection.find().toArray();
+      res.send(result);
+     })
 
    app.get('/toys', async(req, res)=>{
     const cursor = ToyCollection.find();
@@ -45,6 +55,13 @@ async function run() {
       const result = await ToyCollection.insertOne(AddedToys);
       res.send(result);
     })
+
+    app.delete('/toys/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await ToyCollection.deleteOne(query);
+      res.send(result);
+    } )
 
 
 
@@ -67,5 +84,5 @@ app.get('/', (req, res)=> {
 }  ) 
 
 app.listen(port, () => {
-    console.log(`Toyland server is running on port: ${port} `)
+    console.log(`Toyland server is running on port: ${port}`)
 }) 
